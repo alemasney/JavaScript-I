d8c6737ea9ad5b017a83f9b19fc0728e00d3235b
Alan Lemasney <alanlemasney34@yahoo.com>
2019-03-26 22:47:46 -0600

    arrays challenge 3 complete

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -72,8 +72,16 @@ console.log(`${inventory[lastCar]["car_make"]} ${inventory[lastCar]["car_model"]
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModels = [];
-console.log();
+const modelSort = function() {
+    let carModels = [];
+
+    for(let i = 0; i < inventory.length; i++) {
+        carModels.push(inventory[i]["car_model"]);
+        carModels.sort(); 
+    }
+    return carModels;
+}    
+console.log(modelSort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
